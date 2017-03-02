02e261270437844db5af83b13d561fed2eef7034
Paulo Chaves <opaulochaves@gmail.com>
2017-03-02 09:24:08 -0300

    config store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
+import configStore from './store/configStore'
 import App from './App';
-import './index.css';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+import './index.css';
+
+const store = configStore()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
