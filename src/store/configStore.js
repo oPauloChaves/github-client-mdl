345581405b55b57b9087ad1f50d06f27dc3e3a02
Paulo Chaves <opaulochaves@gmail.com>
2017-03-02 09:25:00 -0300

    fix tabs with editorconfig

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -2,28 +2,26 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from '../reducers'
 
-//const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
-
 // See: https://github.com/cloudmu/react-redux-starter-kit/
 export default function configStore(initialState = {}) {
-    let store
+  let store
 
-    if (module.hot) {
-        store = createStore(
-            rootReducer,
-            initialState,
-            compose(
-                applyMiddleware(thunkMiddleware),
-                window.devToolsExtension ? window.devToolsExtension() : f => f
-            )
-        )
-    } else {
-        store = createStore(
-            rootReducer,
-            initialState,
-            compose(applyMiddleware(thunkMiddleware), f => f)
-        )
-    }
+  if (module.hot) {
+    store = createStore(
+      rootReducer,
+      initialState,
+      compose(
+        applyMiddleware(thunkMiddleware),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+      )
+    )
+  } else {
+    store = createStore(
+      rootReducer,
+      initialState,
+      compose(applyMiddleware(thunkMiddleware), f => f)
+    )
+  }
 
-    return store
+  return store
 }
