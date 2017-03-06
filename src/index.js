467f797c288b90763b88aaf1a1c4664c483ebe70
Paulo Chaves <opaulochaves@gmail.com>
2017-03-06 12:36:24 -0300

    remove material-ui

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import configStore from './store/configStore'
 import App from './containers/app/App'
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
 import './index.css';
 
